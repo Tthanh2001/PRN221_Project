b39bc13cd2f0e6f64ed80fd91f70c936be45ec5b
Vuong Quoc Anh K16_HE160809 <115806490+VuongQuocAnhHE160809@users.noreply.github.com>
2023-08-17 23:25:22 +0700

    CRUD Role Admin

---
+++ Areas/Admin/Pages/Role/RolePageModel.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using ProjectPRN.Utils;
+
+namespace PRN221_Project.Areas.Admin.Pages.Role
+{
+    public class RolePageModel:PageModel
+    {
+        protected readonly RoleManager<IdentityRole> _roleManager;
+        protected readonly CinphileDbContext _context;
+        [TempData]
+        public string StatusMessage { get;set; }
+        public RolePageModel(RoleManager<IdentityRole> roleManager, CinphileDbContext context)
+        {
+            _roleManager = roleManager;
+            _context = context;
+        }
+    }
+}
